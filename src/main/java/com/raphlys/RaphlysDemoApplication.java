556aeeb61fa91305ed8840186c451f11ee732d70
pierrelouisgrangier <pierrelouis.grangier@gmail.com>
2025-01-21 12:14:34 +0100

    Init Springboot

---
+++ src/main/java/com/raphlys/RaphlysDemoApplication.java
@@ -0,0 +1,13 @@
+package com.raphlys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaphlysDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaphlysDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphlys/RaphlysDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphlys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaphlysDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
