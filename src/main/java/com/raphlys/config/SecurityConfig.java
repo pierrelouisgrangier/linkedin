1d90af609ddfa3e81c45b9fc9ff901b59708a8e4
pierrelouisgrangier <pierrelouis.grangier@gmail.com>
2025-04-16 22:47:08 +0200

    First docker

--- src/main/java/com/raphlys/config/SecurityConfig.java
+++ src/main/java/com/raphlys/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.raphlys.config;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -30,24 +26,19 @@ public class SecurityConfig {
 		CsrfTokenRequestAttributeHandler handler = new CsrfTokenRequestAttributeHandler();
 		http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				.csrfTokenRequestHandler(handler)
-				.ignoringRequestMatchers(new AntPathRequestMatcher("/login"),
-						new AntPathRequestMatcher("/logout")))
-		.cors(cors -> cors.configurationSource(request -> {
-			CorsConfiguration config = new CorsConfiguration();
-			config.setAllowedOrigins(List.of("http://localhost:4200"));
-			config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-			config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-XSRF-TOKEN", "X-Requested-With"));
-			config.setAllowCredentials(true);
-			return config;
-		})).formLogin(form -> form.successHandler((request, response, authentication) -> {
-					((CsrfToken) request.getAttribute(CsrfToken.class.getName())).getToken();
-					response.setStatus(HttpServletResponse.SC_OK);
-				})
-				.failureHandler((request, response, exception) -> response
-						.setStatus(HttpServletResponse.SC_UNAUTHORIZED)))
-				.authorizeHttpRequests(authorizedHttpRequest -> authorizedHttpRequest
-						.requestMatchers("/raph/all", "GET").permitAll().requestMatchers("/error/**").permitAll().requestMatchers("/raph/**").authenticated())
-				.userDetailsService(customUserDetailService).logout(logout -> logout.logoutUrl("/logout").logoutSuccessHandler(
+				.ignoringRequestMatchers(new AntPathRequestMatcher("/api/login"),
+						new AntPathRequestMatcher("/api/logout")))
+				.formLogin(form -> form.loginProcessingUrl("/api/login").permitAll()
+						.successHandler((request, response, authentication) -> {
+							response.setStatus(HttpServletResponse.SC_OK);
+						})
+						.failureHandler((request, response, exception) -> response
+								.setStatus(HttpServletResponse.SC_UNAUTHORIZED)))
+				.authorizeHttpRequests(
+						authorizedHttpRequest -> authorizedHttpRequest.requestMatchers("/raph/all", "GET").permitAll()
+								.requestMatchers("/error/**").permitAll().requestMatchers("/raph/**").authenticated())
+				.userDetailsService(customUserDetailService)
+				.logout(logout -> logout.logoutUrl("/api/logout").logoutSuccessHandler(
 						(request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK)));
 
 		return http.build();
